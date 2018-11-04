0c079796623f415717a8d4103fb826aaf0c63177
japollo27 <jumpingjon2000@gmail.com>
2018-11-04 15:14:34 -0500

    Updates

---
+++ precache-manifest.09e6a4796df362e41fb04557f7bd2348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e16e7263bff0f80cadf2",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "e16e7263bff0f80cadf2",
+    "url": "/contactmanager/static/js/main.e16e7263.chunk.js"
+  },
+  {
+    "revision": "a3cbe8b63f63d209035e",
+    "url": "/contactmanager/static/css/1.9eabb920.chunk.css"
+  },
+  {
+    "revision": "a3cbe8b63f63d209035e",
+    "url": "/contactmanager/static/js/1.a3cbe8b6.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "e40ae313873ea985c2cb4958ad0ab1d9",
+    "url": "/contactmanager/index.html"
+  }
+];
